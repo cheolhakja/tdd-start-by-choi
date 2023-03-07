9dd7b2c064b690f26fcf3e09ba674b2b974cef69
cheolhakja <rebegin15@gmail.com>
2023-03-07 11:47:41 +0900

    비밀번호 보안 강도 '강함' 에 대한 테스트 작성
    동작하지 않는 테스트 작성

--- src/test/java/tddstart/ch2/stringmeter/PasswordMeterTest.java
+++ src/test/java/tddstart/ch2/stringmeter/PasswordMeterTest.java
@@ -6,5 +6,12 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordMeterTest {
 
+    @Test
+    void meetsAllConditions_Then_Strong() {
+        PasswordMeter meter = new PasswordMeter();
+        PasswordStrength strength = meter.meter("ab12!@AB");
+        assertThat(strength).isEqualTo(PasswordStrength.STRONG)
+    }
+
 
 }
